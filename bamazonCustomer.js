f50bda6249d880666f2b8c1685721f97c50e501a
Nick Grismore <nicksk83@aol.com>
2017-10-10 21:51:43 -0400

    added test nodes

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'bamazon'
+});
+
+connection.connect();
+
+connection.query('SELECT * from < table name >', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
