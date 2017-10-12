9ac9490723c4f9fcec840eaa8c56c248d474eaf5
Nick Grismore <nicksk83@aol.com>
2017-10-12 15:56:57 -0400

    added inquirer and query function for bamazon cust

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,19 +1,51 @@
-var mysql      = require('mysql');
-var express = require('express');
-var app = express();
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-var connection = mysql.createConnection({
+var db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'root',
   database : 'bamazon'
 });
 
-connection.connect(function(err) {
+var products = []
+
+db.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
-  connection.query("CREATE DATABASE bamazon", function (err, result) {
+  db.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    console.log("Database created");
+		for(i = 0; i < res.length; i++) {
+			console.log('ID: ' + res[i].item_id + ' | $' + res[i].price + ' ' + res[i].product_name);
+			products.push(res[i]);
+		};
+	questions();
   });
 });
+
+
+function questions(){
+	inquirer.prompt([
+	{
+		name: 'productID',
+		message: 'What is the ID of the product you want?'
+	},
+	{
+		name: 'units',
+		message: 'How many units would you like?'	
+	}
+
+]).then(function(answer) {
+			var newID = parseInt(answer.id);
+			var idIndex = newID - 1;
+			var chosenUnits = parseInt(answer.units);
+			if(chosenUnits < products[idIndex].stock_quantity){
+				connection.query('UPDATE products SET stock_quantity=stock_quantity-' + chosenUnits + ' WHERE item_id=' + chosenId + ';')
+				connection.query('UPDATE products SET product_sales=' + products[idIndex].price * chosenUnits + ' WHERE item_id=' + chosenId + ';')
+				console.log('Total price: $' + products[idIndex].price * chosenUnits);
+			} else {
+				console.log('Sorry, we ran out :(');
+			}
+		});
+
+
+}
--- bamazontable.sql
+++ bamazontable.sql
@@ -5,3 +5,14 @@ CREATE TABLE products(
 	price VARCHAR(100) NOT NULL,
 	stock_quantity VARCHAR(100) NOT NULL
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('grapes', 'fruit', 10, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('apples', 'fruit', 1, 40);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('pears', 'fruit', 2, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('mangos','fruit', 3, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('oranges','fruit', 2, 30);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('asparagus', 'veggie', 5, 80);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('broccoli', 'veggie', 5, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('celery', 'veggie', 3, 34);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('corn', 'veggie', 2, 45);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('endive', 'veggie', 4, 66);
